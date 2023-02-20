0253c4730706f96cf3756bd80fb97b261e882078
vasily <vasilymishanin@gmail.com>
2023-02-20 12:02:54 +0600

    feat: underfooter

--- src/layout/footer/footer.ts
+++ src/layout/footer/footer.ts
@@ -1,5 +1,6 @@
 import { renderContacts } from "./contacts/contacts";
 import "./footer.css";
+const todayYear = new Date().getFullYear();
 
 const footer = document.createElement("footer");
 footer.classList.add("footer");
@@ -7,3 +8,8 @@ footer.classList.add("footer");
 document.querySelector("#app")!.append(footer);
 
 renderContacts();
+
+footer.innerHTML += `<div class='footer__copyright'>
+<p> made on Vite with vanilla HTML, CSS and TS</p>
+<p> © ${todayYear}</p>
+</div>`;
